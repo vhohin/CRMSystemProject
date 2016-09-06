8e3608e01ea4c3ac04db3db1fd0ec2f3ac690b9d
Nazar Holyanskyy <golyanskiy@gmail.com>
2016-09-06 14:27:31 -0400

    kuku

--- CRM/MainWindow.xaml.cs
+++ CRM/MainWindow.xaml.cs
@@ -27,8 +27,9 @@ namespace CRM
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window2 taskWindow = new Window2();
-            taskWindow.Show();
+           // Window2 taskWindow = new Window2();
+           // taskWindow.Show();
+            MessageBox.Show("kuku");
 
         }
     }
