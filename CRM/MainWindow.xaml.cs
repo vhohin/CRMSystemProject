006aeb843f65e1e5ec49f35a5c8c4c9ec0e3ec54
Valeriy Hohin <vhohin@gmail.com>
2016-09-06 14:24:38 -0400

    ***

--- CRM/MainWindow.xaml.cs
+++ CRM/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace CRM
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window1 taskWindow = new Window1();
+            Window2 taskWindow = new Window2();
             taskWindow.Show();
 
         }
