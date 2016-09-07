7b5ed579b34102b727d722d222767bf26c63ca4c
Nazar Holyanskyy <Nazar Holyanskyy>
2016-09-06 23:10:59 -0400

    no message

---
+++ CRM/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CRM
+{
+    class Database
+    {
+        //
+        const string CONN_STRING = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=F:\BitBuckets\CRMGroupProject\CRM\CRMDB.mdf;Integrated Security=True;Connect Timeout=30";
+        private SqlConnection conn;
+        public Database()
+        {
+            conn = new SqlConnection(CONN_STRING);
+            conn.Open();
+        }
+    }
+}
---
+++ CRM/Employees.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    class Employees
+    {
+            public int EmployeeId { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string Location { get; set; }
+            public string Country { get; set; }
+            public string ZipCode { get; set; }
+            public DateTime DOB { get; set; }
+            public string Phone { get; set; }
+            public DateTime HireDate { get; set; }
+            public int PositionID { get; set; }
+            public int DepartmentID { get; set; }
+            public int Importance { get; set; }
+            public string Description { get; set; }        
+    }
+}
