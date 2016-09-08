2af75f0f28703226bb051ff7c21a1b3e4901d7c3
Nazar Holyanskyy <golyanskiy@gmail.com>
2016-09-08 11:50:15 -0400

    * table Client modified

--- CRM/Clients.cs
+++ CRM/Clients.cs
@@ -13,6 +13,7 @@ namespace CRM
         public string ContactName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
+        public string Location { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
         public string Phone { get; set; }
